7e8e8916a73870fd4358e4f4b7b9df8e6ec5c79c
Aseel3640 <aseelsaifaldin@yahoo.com>
2023-02-20 09:11:55 -0800

    -

---
+++ quick_sort.c
@@ -0,0 +1,68 @@
+#include "sort.h"
+/**
+ * swap_items -swap 2 items in an array
+ * @array: the array
+ * @l: the idx of the left item
+ * @r: the idx of the right item
+ */
+void swap_items(int *array, size_t l, size_t r)
+{
+	int tmp;
+
+	if (array != NULL)
+	{
+		tmp = array[l];
+		array[l] = array[r];
+		array[r] = tmp;
+	}
+}
+/**
+ * quick_sort_range_lomuto - sorts a sub array using the
+ * q a log
+ * @array: the array containing the sub array
+ * @high: the ending position
+ * @size: the length of array
+ */
+void quick_sort_range_lomuto(int *array, size_t low size_t high, size_t size)
+{
+	size_t k, i;
+	int pivot;
+
+	if ((low >= high || (array == NULL))
+		return;
+	pivot = array[high];
+	k = low;
+	for (i = low; i < high; i++)
+	{
+		if (array[i] <= pivot)
+		{
+			if (k != i)
+			{
+				swap_items(array, k, i);
+				print_array(array, size);
+			}
+			k++
+		}
+	}
+	if (k != high)
+	{
+		swap_items(array, k, high);
+		print_array(array, size);
+	}
+	if (k - low > 1)
+		quick_sort_range_lomuto(array, low k - 1, size);
+	if (high - k > 1)
+		quick_sort_range_lomuto(array, k + 1, high, size);
+}
+/**
+ * quick_sort - sorts an array
+ * @array: the array
+ * @size: the length of the array
+ */
+void quick_sort(int *array, size_t size)
+{
+	if (array != NULL)
+	{
+		quick_sort_range_lomuto(array, 0 size - 1, size);
+	}
+}
